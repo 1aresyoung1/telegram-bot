b3990a35677e5e45b3ccc657fffcc4b389bfbe1c
1aresyoung1 <bolotovbogdan0602@gmail.com>
2025-12-29 10:18:10 +0100

    Remove start command handler from main.py
    Removed the start command handler from the bot.

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from telegram.ext import Application, CommandHandler
 import os
+from telegram.ext import Application, CommandHandler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update, context):
-    await update.message.reply_text("Бот працює ✅")
-
 def main():
     app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
 if __name__ == "__main__":
